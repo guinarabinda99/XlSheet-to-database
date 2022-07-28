f49fdb5ace04d9132afb19ab82cc70717be2d9d2
arabinda.guin <arabinda.guin@CSMBHUL511.corp.csmpl.com>
2022-07-28 11:42:55 +0530

    updated by Arabinda

--- XlsheetToDataBase/src/main/java/com/csm/demo/Controller/PackageController.java
+++ XlsheetToDataBase/src/main/java/com/csm/demo/Controller/PackageController.java
@@ -6,7 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,17 +17,23 @@ import com.csm.demo.service.PackageService;
 
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/api")
 public class PackageController {
 	
 	@Autowired
 	private PackageService packageService;
 	
-	@PostMapping("/product/upload")
+	@RequestMapping(value = "/hello",method = RequestMethod.GET)
+	public String check() {
+		return "Hello";
+		
+	}
+	
+	@RequestMapping(value = "/upload",method = RequestMethod.POST)
 	public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file){
 		
 		if(HelperClass.checkExcelFormat(file)) {
 			packageService.save(file);
-			System.out.println("shhs");
 			return ResponseEntity.ok(Map.of("Message","File is Uploaded Successfully into Database"));
 		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please Upload Excel File Only");
